6e1532bf4f828f1cf21fb99811bf8a240243e58a
JSGandalf <jinmayjhaveri006@gmail.com>
2022-05-18 11:35:05 +0530

    feat: stuff works. refactoring needed

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,7 @@
-import { parse } from "@babel/parser";
-import traverse, { TraverseOptions } from "@babel/traverse";
+import { transform } from "@babel/core";
+import { TraverseOptions } from "@babel/traverse";
 import * as t from "@babel/types";
+import { identifier } from "@babel/types";
 import { createUnplugin } from "unplugin";
 
 function isReactComponent(id: string) {
@@ -31,28 +32,51 @@ function isReactAddDepHook(id: string) {
 function returnComponentVisitor(props: Record<string, string>) {
   const identifiers: t.Identifier[] = [];
   return {
-    VariableDeclarator: {
-      enter(path) {
-        if (
-          t.isCallExpression(path.node.init) &&
-          t.isIdentifier(path.node.init.callee)
-        ) {
-          const {
-            callee: { name },
-          } = path.node.init;
-          if (isReactIdentifierRecordHook(name)) {
-            if (t.isArrayPattern(path.node.id)) {
-              identifiers.push(...(path.node.id.elements as t.Identifier[]));
-            }
-          } else if (isReactAddDepHook(name)) {
-            // well figure out what is used and add it
-            const stuff = t.getBindingIdentifiers(path.node.init);
-            console.log(stuff);
+    VariableDeclarator(path) {
+      if (
+        t.isCallExpression(path.node.init) &&
+        t.isIdentifier(path.node.init.callee)
+      ) {
+        const {
+          callee: { name },
+        } = path.node.init;
+        if (isReactIdentifierRecordHook(name)) {
+          if (t.isArrayPattern(path.node.id)) {
+            identifiers.push(...(path.node.id.elements as t.Identifier[]));
           }
-          console.log(name);
-          console.log(identifiers);
+        } else if (isReactAddDepHook(name)) {
+          // well figure out what is used and add it
+          const node = path.node.init;
+          if (
+            (t.isArrowFunctionExpression(node.arguments[0]) ||
+              t.isFunctionExpression(node.arguments[0])) &&
+            t.isArrayExpression(node.arguments[1])
+          ) {
+            // extract identifiers used from node.arguments[0]
+            console.log("proved true");
+            t.traverse(node.arguments[0], {
+              enter(identifierNode) {
+                if (
+                  t.isIdentifier(identifierNode) &&
+                  identifiers.some((identifier) =>
+                    t.isNodesEquivalent(identifier, identifierNode),
+                  )
+                ) {
+                  node.arguments[1].elements.push(identifierNode);
+                }
+              },
+            });
+          }
         }
-      },
+      }
+    },
+    CallExpression(path) {
+      if (t.isIdentifier(path.node.callee)) {
+        const { name } = path.node.callee;
+        if (isReactAddDepHook(name)) {
+          //figuring out what is used and using it
+        }
+      }
     },
   } as TraverseOptions<unknown>;
 }
@@ -61,22 +85,25 @@ const plugins = createUnplugin((opts) => {
   return {
     name: "unplugin-add-deps",
     transformInclude(id) {
-      return /\.(j|t)sx/.test(id);
+      return /\.(j|t)sx$/.test(id);
     },
     transform(code: string) {
-      const ast = parse(code, { sourceType: "module" });
-      const identifiers: t.Identifier[] = [];
-      let readAssignments = true;
-
-      traverse(ast, {
-        FunctionDeclaration(path) {
-          const { id } = path.node;
-          if (t.isIdentifier(id) && isReactComponent(id.name)) {
-            path.traverse(returnComponentVisitor({}));
-          }
-        },
+      const { code: code1 } = transform(code, {
+        plugins: [
+          {
+            visitor: {
+              FunctionDeclaration(path) {
+                const { id } = path.node;
+                if (t.isIdentifier(id) && isReactComponent(id.name)) {
+                  path.traverse(returnComponentVisitor({}));
+                }
+              },
+            },
+          },
+        ],
       });
-      return code;
+      console.log(code1);
+      return code1;
     },
   };
 });
