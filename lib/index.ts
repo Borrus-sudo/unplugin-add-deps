99c80d182c02899a2552e30802ee09aadb573f2a
JSGandalf <jinmayjhaveri006@gmail.com>
2022-05-18 12:08:17 +0530

    feat: finishing touches

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,6 @@
 import { transform } from "@babel/core";
 import { TraverseOptions } from "@babel/traverse";
 import * as t from "@babel/types";
-import { identifier } from "@babel/types";
 import { createUnplugin } from "unplugin";
 
 function isReactComponent(id: string) {
@@ -29,8 +28,31 @@ function isReactAddDepHook(id: string) {
   ].includes(id);
 }
 
-function returnComponentVisitor(props: Record<string, string>) {
-  const identifiers: t.Identifier[] = [];
+function isFunctionExpress(node) {
+  return t.isArrowFunctionExpression(node) || t.isFunctionExpression(node);
+}
+
+function addDepsToHook(node, identifiers) {
+  if (
+    isFunctionExpress(node.arguments[0]) &&
+    t.isArrayExpression(node.arguments[1])
+  ) {
+    t.traverse(node.arguments[0], {
+      enter(identifierNode) {
+        if (
+          identifiers.some((identifier) =>
+            t.isNodesEquivalent(identifier, identifierNode),
+          )
+        ) {
+          node.arguments[1].elements.push(identifierNode);
+        }
+      },
+    });
+  }
+}
+
+function returnComponentVisitor(props) {
+  const identifiers: t.Identifier[] = [...props];
   return {
     VariableDeclarator(path) {
       if (
@@ -44,42 +66,47 @@ function returnComponentVisitor(props: Record<string, string>) {
           if (t.isArrayPattern(path.node.id)) {
             identifiers.push(...(path.node.id.elements as t.Identifier[]));
           }
-        } else if (isReactAddDepHook(name)) {
-          // well figure out what is used and add it
-          const node = path.node.init;
-          if (
-            (t.isArrowFunctionExpression(node.arguments[0]) ||
-              t.isFunctionExpression(node.arguments[0])) &&
-            t.isArrayExpression(node.arguments[1])
-          ) {
-            // extract identifiers used from node.arguments[0]
-            console.log("proved true");
-            t.traverse(node.arguments[0], {
-              enter(identifierNode) {
-                if (
-                  t.isIdentifier(identifierNode) &&
-                  identifiers.some((identifier) =>
-                    t.isNodesEquivalent(identifier, identifierNode),
-                  )
-                ) {
-                  node.arguments[1].elements.push(identifierNode);
-                }
-              },
-            });
-          }
         }
       }
     },
     CallExpression(path) {
+      //path.node is FunctionNode
       if (t.isIdentifier(path.node.callee)) {
         const { name } = path.node.callee;
-        if (isReactAddDepHook(name)) {
+        const comment = path.node?.leadingComments?.[0];
+        if (isReactAddDepHook(name) && comment?.value !== "ignore") {
           //figuring out what is used and using it
+          addDepsToHook(path.node, identifiers);
         }
       }
     },
   } as TraverseOptions<unknown>;
 }
+
+export const createBabelPlugin = () => {
+  return {
+    visitor: {
+      FunctionDeclaration(path) {
+        const { id } = path.node;
+        if (t.isIdentifier(id) && isReactComponent(id.name)) {
+          const props = [];
+          path.node.params.forEach((param) => {
+            t.traverse(param, {
+              enter(propsNode) {
+                if (t.isIdentifier(propsNode)) {
+                  props.push(propsNode);
+                }
+              },
+            });
+          });
+
+          console.log(props);
+          path.traverse(returnComponentVisitor(props));
+        }
+      },
+    },
+  };
+};
 
 const plugins = createUnplugin((opts) => {
   return {
@@ -88,24 +115,17 @@ const plugins = createUnplugin((opts) => {
       return /\.(j|t)sx$/.test(id);
     },
     transform(code: string) {
-      const { code: code1 } = transform(code, {
-        plugins: [
-          {
-            visitor: {
-              FunctionDeclaration(path) {
-                const { id } = path.node;
-                if (t.isIdentifier(id) && isReactComponent(id.name)) {
-                  path.traverse(returnComponentVisitor({}));
-                }
-              },
-            },
-          },
-        ],
+      const { code: transformedCode } = transform(code, {
+        plugins: [createBabelPlugin()],
       });
-      console.log(code1);
-      return code1;
+      console.log(transformedCode);
+      return transformedCode;
     },
   };
 });
 
 export const vite = plugins.vite;
+export const esbuild = plugins.esbuild;
+export const rollup = plugins.rollup;
+export const webpack = plugins.webpack;
+export const babel = createBabelPlugin();
--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,11 +1,13 @@
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+function App({ hello }) {
   const [count, setCount] = useState(0);
   const memoed = useMemo(() => count, []);
-
+  useEffect(() => {
+    console.log(hello);
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
