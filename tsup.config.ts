6189a5fac75484b8f3ae329a3ae2b806a7b1ca19
JSGandalf <jinmayjhaveri006@gmail.com>
2022-05-18 16:41:54 +0530

    chore: update build

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,13 @@
-import { Options } from "tsup"
+import { Options } from "tsup";
 
 const config: Options = {
   splitting: true,
   format: ["esm", "cjs"],
   entryPoints: ["lib/index.ts"],
   clean: true,
-  dts: true,
+  dts: false,
+  minify: true,
   outDir: "./out",
-}
+};
 
-export default config
+export default config;
